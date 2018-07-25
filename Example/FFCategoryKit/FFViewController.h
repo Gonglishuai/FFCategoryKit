ddbba6e6b041a4b1763ffb553b836d0c0a020f12
gonglishuai <gonglishuai@juran.com.cn>
2018-07-25 21:41:41 +0800

    Initial commit

---
+++ Example/FFCategoryKit/FFViewController.h
@@ -0,0 +1,13 @@
+//
+//  FFViewController.h
+//  FFCategoryKit
+//
+//  Created by gonglishuai on 07/25/2018.
+//  Copyright (c) 2018 gonglishuai. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FFViewController : UIViewController
+
+@end
---
+++ Example/FFCategoryKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FFCategoryKit
+//
+//  Created by gonglishuai on 07/25/2018.
+//  Copyright (c) 2018 gonglishuai. All rights reserved.
+//
+
+@import UIKit;
+#import "FFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FFAppDelegate class]));
+    }
+}
